2a3ebedfd5bf0965384daa1db5984435393b875d
Jhojam1 <JhojamCaraballo@gmail.com>
2025-05-08 00:37:56 -0500

    Se Act

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@
  */
 
 // Probar con la URL directa nuevamente (después de configurar CORS en el backend)
-const API_BASE_URL = '/api';
+const API_BASE_URL = 'https://mipaginaprueba.rf.gd/api';
 
 
 export { API_BASE_URL };
