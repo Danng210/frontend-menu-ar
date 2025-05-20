7502bc189cfc845deb7c9059967a01b4fedcfe0a
Danng210 <danielamercadog21@gmail.com>
2025-05-19 22:40:47 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@
 
 // Probar con la URL directa nuevamente (después de configurar CORS en el backend)
 // const API_BASE_URL = 'https://hamburger3d.infinityfreeapp.com/api';
-const API_BASE_URL = 'lightyellow-skunk-728129.hostingersite.com/api';
+const API_BASE_URL = 'https://lightyellow-skunk-728129.hostingersite.com/api';
 export { API_BASE_URL };
