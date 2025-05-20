1334339ce196d450621611258566fbe63d2d359f
Danng210 <danielamercadog21@gmail.com>
2025-05-19 22:25:15 -0500

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@
 
 // Probar con la URL directa nuevamente (después de configurar CORS en el backend)
 // const API_BASE_URL = 'https://hamburger3d.infinityfreeapp.com/api';
-const API_BASE_URL = 'https://auth-db719.hstgr.io/index.php?route=/database/structure&db=u896684805_burger';
+const API_BASE_URL = 'lightyellow-skunk-728129.hostingersite.com/api';
 export { API_BASE_URL };
